129058e38111c93214f393f5039cdcd1842527c4
nomadic4life <orbison.dennis@gmail.com>
2024-07-12 16:25:20 -0700

    added raydium state

--- programs/dao_vote_program/src/states/mod.rs
+++ programs/dao_vote_program/src/states/mod.rs
@@ -4,6 +4,15 @@ pub use program_authority::*;
 pub mod status;
 pub use status::*;
 
+pub mod oracle;
+pub use oracle::*;
+
+pub mod pool;
+pub use pool::*;
+
+pub mod config;
+pub use config::*;
+
 // locking programing tokens
 //  - to earn tokens from tax coming from voting
 //  - to earn yield generated from treasury investments
